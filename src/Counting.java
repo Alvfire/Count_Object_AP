2d9789328c413730dbc920e2be7e07d6815721ff
Alvfire <pelaar@farmingdale.edu>
2024-02-18 20:19:51 -0500

    Counting class set up with static int count

---
+++ src/Counting.java
@@ -0,0 +1,8 @@
+public class Counting {
+    static int count = 0;
+public Counting(){
+
+    count++;
+
+}
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+    Counting count1 = new Counting();
+    Counting count2 = new Counting();
+
+        System.out.println("Number of objects using static data member: "+Counting.count);
+
+    }
+}
